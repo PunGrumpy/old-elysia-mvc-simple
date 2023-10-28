8c2d07b68305db9c37c71f7dd109d49dde3f6efc
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-10-29 00:16:58 +0700

    feat(src): add example swagger to use with elysia

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import swagger from '@elysiajs/swagger'
 import { Elysia } from 'elysia'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-)
+const app: Elysia = new Elysia()
+  .use(swagger())
+  .get(
+    '/',
+    () => `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  )
+  .listen(3000)
