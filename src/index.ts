80d736fe3396deb6f908cb2727f26321f9fda0be
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-10-29 15:20:33 +0700

    fix(*): update doc for swagger

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,23 @@ import {
 } from './controller/user'
 
 const app: Elysia = new Elysia()
-  .use(swagger())
+  .use(
+    swagger({
+      exclude: ['/swagger', '/'],
+      autoDarkMode: true,
+      documentation: {
+        info: {
+          title: '🦊 Elysia MVC Simple',
+          description: 'Simple MVC pattern for ElysiaJS with User and Post',
+          version: '1.0.0',
+          license: {
+            name: 'MIT',
+            url: 'https://opensource.org/license/mit/'
+          }
+        }
+      }
+    })
+  )
   .get('/', ({ set }) => (set.redirect = '/swagger'))
   .get('/users', async ({ set }) => {
     try {
