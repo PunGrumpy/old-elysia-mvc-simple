c7e099107e7348e44b8aba2698c801f4d2f3ac1e
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-10-29 09:49:54 +0700

    fix(*): set redirect to swagger

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import { getAllUsers, getUserById } from './controller/user'
 
 const app: Elysia = new Elysia()
   .use(swagger())
-  .get(
-    '/',
-    () => `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-  )
+  .get('/', ({ set }) => (set.redirect = '/swagger'))
   .get('/users', getAllUsers)
   .get('/users/:id', ({ params }: { params: { id: string } }) => {
     const { id } = params
     return getUserById(id)
   })
   .listen(3000)
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+)
