2dd94585a457b62e49b64e356b4c039686906de0
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-10-30 08:52:02 +0700

    fix(vercel): update function name

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import '../src/env' // validate environment variables
-import { app } from '../src/api'
+import { APIRoute } from '../src/api'
 
 export const config = { runtime: 'edge' }
 
 export default async function handler(request: Request) {
-  return app.fetch(request)
+  return APIRoute.fetch(request)
 }
