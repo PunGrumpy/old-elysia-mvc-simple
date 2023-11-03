eff2f9c084f201c1eb89ec9bd2b3193ab4bcbcbb
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-11-04 00:45:25 +0700

    style(*): make output on terminal look good

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  ELYSIA_VERSION: z.string().default('0.0.0'),
   PORT: z.string().default('3000'),
   RUNTIME: z.enum(['bun', 'edge']).default('bun'),
   NODE_ENV: z.enum(['development', 'production']).default('development')
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,25 @@ const server = CreateElysia()
 
 server.listen({ port: env.PORT }, ({ hostname, port }) => {
   const url = env.NODE_ENV === 'production' ? 'https' : 'http'
+  process.stdout.write('\x1Bc')
 
-  console.log(`🦊 Elysia is running at ${url}://${hostname}:${port}`)
+  const message = `🦊 Elysia is running at ${url}://${hostname}:${port}`
+  const title = `Elysia v${env.ELYSIA_VERSION}`
+  const boxWidth = Math.max(message.length, title.length) + 4
+  const border = '─'.repeat(boxWidth)
+
+  const centerPad = (text: string) => {
+    const padding = ' '.repeat((boxWidth - text.length) / 2)
+    return `${padding}${text}${padding}`
+  }
+
+  console.log(`
+  ┌${border}┐
+  │${centerPad('')} │
+  │${centerPad(title)}│
+  │${centerPad('')} │
+  │${centerPad(message)}│
+  │${centerPad('')} │
+  └${border}┘
+  `)
 })
