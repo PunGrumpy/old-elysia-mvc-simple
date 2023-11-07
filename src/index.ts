047ca7d9fbaac717641f93b66aacbe46aa467137
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-11-07 21:33:14 +0700

    fix(*): update logger (logixlysia) new version

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { env } from './env'
 import { app } from './app'
 import { CreateElysia } from './utils/elysia'
 import { contextRequest } from './utils/contextRequest'
-import { logging } from './plugin/logging'
 import { logger } from 'logixlysia'
 
 const server = CreateElysia()
@@ -10,11 +9,4 @@ const server = CreateElysia()
   .use(logger())
   .use(app)
 
-server.listen({ port: env.PORT }, ({ hostname, port }) => {
-  const url = env.NODE_ENV === 'production' ? 'https' : 'http'
-  process.stdout.write('\x1Bc')
-
-  const message = `🦊 Elysia is running at ${url}://${hostname}:${port}`
-  const title = `Elysia v${env.ELYSIA_VERSION}`
-  logging(message, title)
-})
+server.listen({ port: env.PORT })
